815b34583976f42ceffcb1ad0ca2f38414a0a40e
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2025-04-21 16:29:23 -0500

    Arquitectura inicial del test.

--- test_crear_dia_libre.py
+++ test_crear_dia_libre.py
@@ -31,13 +31,27 @@ def test_crear_dia_usuario_valido(page: Page) -> None:
     page.locator("#select2-type-container").click()
     page.get_by_role("option", name="Baja por enfermedad").click()
 
-    # Seleccionando las fechas
+    # Seleccionando la fecha de inicio
     page.locator("#date_debut_").fill("06/04/2025")
+    page.locator("#select2-starthalfday-container").click()
+    page.get_by_role("option", name="Mañana").click()
+
+    # Seleccionando la fecha de fin
     page.locator("#date_fin_").fill("06/10/2025")
+    page.locator("#select2-endhalfday-container").click()
+    page.get_by_role("option", name="Tarde").click()
 
     # Seleccionando el revisor
     page.locator("span[id='select2-valideur-container']").click()
     page.get_by_role("option", name="Revisor Vacaciones").click()
+
+    # Botón fuente HTML
+    page.locator("#cke_46").click()
+
+    # Colocando texto en la descripción (HTML)
+    page.locator(
+        "xpath=//textarea[@class='cke_source cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr']"
+        ).fill("בב")
 
     # Clic en el botón de guardado
     page.get_by_role("button", name="Crear solicitud de licencia").click()
@@ -48,6 +62,7 @@ def test_crear_dia_usuario_valido(page: Page) -> None:
     expect(page.get_by_text("06/04/2025")).to_be_visible()
     expect(page.get_by_text("06/10/2025")).to_be_visible()
     expect(page.get_by_text("Revisor Vacaciones")).to_be_visible()
+    expect(page.get_by_text("בב")).to_be_visible()
 
     # Verificando la marca de tiempo de grabación
     tiempo = page.locator("span[class=opacitymedium]").all()
