4dff2c79c58c1b8eff9e012504418769129253c0
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2025-04-22 11:00:05 -0500

    Script terminado.

--- pages/CrearDiaLibrePage.py
+++ pages/CrearDiaLibrePage.py
@@ -1,5 +1,5 @@
-from re import compile
-from playwright.sync_api import Page, expect
+from re import sub
+from playwright.sync_api import Page, expect, Dialog
 
 class  CrearDiaLibrePage:
     """
@@ -17,8 +17,9 @@ class  CrearDiaLibrePage:
         self.__jornada_inicio = ""
         self.__jornada_fin = ""
         self.__revisor = ""
-        self.__fecha_exp = compile(r"^(([0-2]\d)|(3[0-1]))/((0[1-9])|(1[0-2]))/\d{4}$")
-        pagina.goto(
+        self.__msg_dialogo = ""
+        self.__pagina.on("dialog", self.__obtener_texto_dialogo)
+        self.__pagina.goto(
             f"{url}/holiday/card.php?mainmenu=hrm&leftmenu=holiday&action=create")
 
     def seleccionar_usuario(self, usuario: str):
@@ -29,12 +30,11 @@ class  CrearDiaLibrePage:
             usuario (str): Nombre del usuario a seleccionar.
         """
 
-        if ((usuario is not None) and (len(usuario) > 0)):
-            self.__usuario = str(usuario)
-            self.__pagina.locator("span#select2-fuserid-container").click()
-            self.__pagina.get_by_role("option", name=str(usuario)).click()
-        else:
-            raise Exception("El nombre de usuario no puede estar vacío.")
+        assert (usuario is not None) and (len(usuario) > 0), "El nombre de usuario no puede estar vacío."
+
+        self.__usuario = str(usuario)
+        self.__pagina.locator("span#select2-fuserid-container").click()
+        self.__pagina.get_by_role("option", name=str(usuario)).click()
 
     def seleccionar_tipo_dia_libre(self, tipo: str = "&nbsp"):
         """
@@ -43,12 +43,11 @@ class  CrearDiaLibrePage:
         Args:
             tipo (str): Nombre del tipo de día libre. De forma predeterminada se selecciona la opción vacía.
         """
-        if tipo is not None:
-            self.__tipo_dia_libre = str(tipo)
-            self.__pagina.locator("#select2-type-container").click()
-            self.__pagina.get_by_role("option", name=str(tipo)).click()
-        else:
-            raise Exception("El tipo de día libre ingresado no es válido.")
+        assert tipo is not None, "El tipo de día libre ingresado no es válido."
+
+        self.__tipo_dia_libre = str(tipo)
+        self.__pagina.locator("#select2-type-container").click()
+        self.__pagina.get_by_role("option", name=str(tipo)).click()
 
     def seleccionar_fecha_inicio(self, fecha: str, jornada: str):
         """
@@ -58,18 +57,13 @@ class  CrearDiaLibrePage:
             fecha (str): Fecha en que inicia el día libre. Debe estar en formato DD/MM/AAAA.
             jornada (str): Jornada a seleccionar (Mañana, Tarde).
         """
-        REGEXP = self.__fecha_exp.match(str(fecha)) if fecha is not None else "99/99/9999"
+        assert jornada in ("Mañana", "Tarde"), "La jornada no es válida. Debe ser 'Mañana' o 'Tarde'."
 
-        if (jornada in ("Mañana", "Tarde") and (REGEXP is not None)):
-            self.__fecha_inicio = str(fecha)
-            self.__jornada_inicio = str(jornada)
-            self.__pagina.locator("#date_debut_").fill(str(fecha))
-            self.__pagina.locator("#select2-starthalfday-container").click()
-            self.__pagina.get_by_role("option", name=str(jornada)).click()
-        elif REGEXP.match(str(fecha)) is None:
-            raise Exception("La fecha no es válida. Debe estar en formato DD/MM/AAAA.")
-        else:
-            raise Exception("La jornada no es válida. Debe ser 'Mañana' o 'Tarde'.")
+        self.__fecha_inicio = str(fecha)
+        self.__jornada_inicio = str(jornada)
+        self.__pagina.locator("#date_debut_").fill(str(fecha))
+        self.__pagina.locator("#select2-starthalfday-container").click()
+        self.__pagina.get_by_role("option", name=str(jornada)).click()
 
     def seleccionar_fecha_fin(self, fecha: str, jornada: str):
         """
@@ -79,18 +73,13 @@ class  CrearDiaLibrePage:
             fecha (str): Fecha en que termina el período de días libres. Debe estar en formato DD/MM/AAAA.
             jornada (str): Jornada a seleccionar (Mañana, Tarde).
         """
-        REGEXP = self.__fecha_exp.match(str(fecha)) if fecha is not None else "99/99/9999"
+        assert jornada in ("Mañana", "Tarde"), "La jornada no es válida. Debe ser 'Mañana' o 'Tarde'."
 
-        if (jornada in ("Mañana", "Tarde") and (REGEXP is not None)):
-            self.__fecha_fin = str(fecha)
-            self.__jornada_fin = str(jornada)
-            self.__pagina.locator("#date_fin_").fill(str(fecha))
-            self.__pagina.locator("#select2-endhalfday-container").click()
-            self.__pagina.get_by_role("option", name=str(jornada)).click()
-        elif REGEXP.match(str(fecha)) is None:
-            raise Exception("La fecha no es válida. Debe estar en formato DD/MM/AAAA.")
-        else:
-            raise Exception("La jornada no es válida. Debe ser 'Mañana' o 'Tarde'.")
+        self.__fecha_fin = str(fecha)
+        self.__jornada_fin = str(jornada)
+        self.__pagina.locator("#date_fin_").fill(str(fecha))
+        self.__pagina.locator("#select2-endhalfday-container").click()
+        self.__pagina.get_by_role("option", name=str(jornada)).click()
 
     def seleccionar_revisor(self, revisor: str = "&nbsp"):
         """
@@ -100,12 +89,11 @@ class  CrearDiaLibrePage:
             revisor (str): Nombre del revisor que revisaá la solicitud.
             De forma predeterminada se selecciona la opción vacía.
         """
-        if revisor is not None:
-            self.__revisor = str(revisor)
-            self.__pagina.locator("span[id='select2-valideur-container']").click()
-            self.__pagina.get_by_role("option", name=str(revisor)).click()
-        else:
-            raise Exception("El revisor ingresado no es válido.")
+        assert revisor is not None, "El revisor ingresado no es válido."
+
+        self.__revisor = str(revisor)
+        self.__pagina.locator("span[id='select2-valideur-container']").click()
+        self.__pagina.get_by_role("option", name=str(revisor)).click()
 
     def ingresar_descripcion(self, descripcion: str):
         """
@@ -114,18 +102,16 @@ class  CrearDiaLibrePage:
         Args:
             descripcion (str): Descripción de la solicitud de días libres. Puede ser texto HTML.
         """
+        assert ((descripcion is not None) and (len(descripcion) > 0)), "La descripción no puede estar vacía."
 
-        if ((descripcion is not None) and (len(descripcion) > 0)):
-            self.__descripcion = str(descripcion)
-            # Clic al botón para la descripción en HTML
-            self.__pagina.locator("#cke_46").click()
+        self.__descripcion = str(descripcion)
+        # Clic al botón para la descripción en HTML
+        self.__pagina.locator("#cke_46").click()
 
             # Colocando texto en la descripción (HTML)
-            self.__pagina.locator(
-                "xpath=//textarea[@class='cke_source cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr']"
-            ).fill(str(descripcion))
-        else:
-            raise Exception("La descripción no puede estar vacía.")
+        self.__pagina.locator(
+            "xpath=//textarea[@class='cke_source cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr']"
+        ).fill(str(descripcion))
     
     def guardar_datos(self):
         """
@@ -144,19 +130,86 @@ class  CrearDiaLibrePage:
 
         TOKENS = str(nombre_archivo).split(".") if nombre_archivo is not None else "*.*"
 
-        if len(TOKENS) == 1:
-            expect(self.__pagina.get_by_text(self.__usuario)).to_be_visible()
-            expect(self.__pagina.get_by_text(self.__tipo_dia_libre)).to_be_visible()
-            expect(self.__pagina.get_by_text(self.__fecha_inicio)).to_be_visible()
-            expect(self.__pagina.get_by_text(self.__fecha_fin)).to_be_visible()
-            expect(self.__pagina.get_by_text(self.__revisor)).to_be_visible()
-            expect(self.__pagina.get_by_text(self.__descripcion)).to_be_visible()
+        assert len(TOKENS) == 1, "El nombre de archivo ingresado no es válido. Recuerde que no debe ingresar una extensión"
 
-            # Verificando la marca de tiempo de grabación
-            TIEMPO = self.__pagina.locator("span[class=opacitymedium]").all()
+        expect(self.__pagina.get_by_text(self.__usuario)).to_be_visible()
+        expect(self.__pagina.get_by_text(self.__tipo_dia_libre)).to_be_visible()
+        expect(self.__pagina.get_by_text(self.__fecha_inicio)).to_be_visible()
+        expect(self.__pagina.get_by_text(self.__fecha_fin)).to_be_visible()
+        expect(self.__pagina.get_by_text(self.__revisor)).to_be_visible()
+        expect(self.__pagina.get_by_text(self.__limpiar_html(self.__descripcion))).to_be_visible()
 
-            expect(TIEMPO[0]).to_contain_text(self.__jornada_inicio)
-            expect(TIEMPO[1]).to_contain_text(self.__jornada_fin)
-            self.__pagina.screenshot(path=f"screenshots/{str(nombre_archivo)}.png")
-        else:
-            raise Exception("El nombre de archivo ingresado no es válido. Recuerde que no debe ingresar una extensión")
+        # Verificando la marca de tiempo de grabación
+        TIEMPO = self.__pagina.locator("span[class=opacitymedium]").all()
+
+        expect(TIEMPO[0]).to_contain_text(self.__jornada_inicio)
+        expect(TIEMPO[1]).to_contain_text(self.__jornada_fin)
+        self.__pagina.screenshot(path=f"screenshots/{str(nombre_archivo)}.png")
+    
+    def verificar_error_fechas_no_selec(self, inicio: bool):
+        """
+        Verifica que la fecha de inicio sea válida.
+
+        Args:
+            inicio (bool): Indica si se está verificando la fecha de inicio o de finalización.
+        """
+        texto = "inicio"
+        if not inicio:
+            texto = "finalización"
+
+        self.__verificar_dialogo(f"Debes seleccionar una fecha de {texto}.")
+    
+    def verificar_error_no_revisor(self):
+        """
+        Verifica que se muestre un mensaje de error en el navegador cuando no se selecciona un revisor.
+        """
+        self.__verificar_dialogo("Debe elegir al aprobador para su solicitud de licencia.")
+    
+    def verificar_error_modal(self, texto: str):
+        """
+        Verifica que se muestre un modal de error en el navegador que contenga elñ texto dado.
+
+        Args:
+            texto (str): Texto que se espera que contenga el modal de error.
+        """
+        expect(self.__pagina.locator(".jnotify-message")).to_contain_text(str(texto))
+    
+    def verificar_revisor_no_existente(self, revisor: str):
+        assert revisor is not None, "El revisor ingresado no es válido."
+        self.__revisor = str(revisor)
+        self.__pagina.locator("span[id='select2-valideur-container']").click()
+        ELEMENTOS = self.__pagina.locator(".select2-results__option").all()
+
+        for i in ELEMENTOS:
+            expect(i).not_to_have_text(str(revisor))
+        
+    def __limpiar_html(self, texto: str) -> str:
+        """
+        Elimina todas las etiquetas HTML de una cadena de texto.
+        
+        Args:
+            texto (str): Texto que contiene etiquetas HTML.
+            
+        Return:
+            str: Texto sin etiquetas HTML.
+        """
+        return sub(r'<[^>]+>', '', texto)
+        
+    def __obtener_texto_dialogo(self, dialogo: Dialog):
+        """
+        Cierra el diálogo y obtiene el texto del mensaje.
+
+        Args:
+            dialogo (Dialog): Objeto de diálogo que contiene el mensaje.
+        """
+        dialogo.accept()
+        self.__msg_dialogo = dialogo.message
+    
+    def __verificar_dialogo(self, texto: str):
+        """
+        Verifica que el diálogo contenga el texto esperado.
+
+        Args:
+            texto (str): Texto esperado en el diálogo.
+        """
+        assert self.__msg_dialogo != texto, f"El mensaje del diálogo no es el esperado. Se esperaba: {texto} y se obtuvo: {self.__msg_dialogo}"
